0ef5a80a954fab11cd3fa0b978864703c066ad80
Amandininio <amandiomota@gmail.com>
2019-08-26 10:02:21 +0200

    bug Fixed

--- src/app/places/places-routing.module.ts
+++ src/app/places/places-routing.module.ts
@@ -6,9 +6,12 @@ const routes: Routes = [
     {   path: 'tabs',
         component: PlacesPage,
         children: [
-            {path: 'discover', children: [
-                {path: '',
-                loadChildren: './discover/discover.module#DiscoverPageModule'
+            {
+                path: 'discover',
+                children: [
+                {
+                    path: '',
+                    loadChildren: './discover/discover.module#DiscoverPageModule'
                 },
                 {
                     path: ':placeId',
@@ -16,22 +19,25 @@ const routes: Routes = [
                 }
             ]},
             {
-                path: 'offers', children: [
-                    {path: '',
-                    loadChildren: './offers/offers.module#OffersPageModule'
-                },
-                {
-                    path: 'new',
-                    loadChildren: './offers/new-offer/new-offer.module#NewOfferPageModule'
-                },
-                {
-                    path: 'edit/:placeId',
-                    loadChildren: './offers/edit-offer/edit-offer.module#EditOfferPageModule'
-                },
-                {
-                    path: ':placeId',
-                    loadChildren: './offers/offer-bookings/offer-bookings.module#OfferBookingsPageModule'
-                }
+                path: 'offers',
+                children:
+                [
+                    {
+                        path: '',
+                        loadChildren: './offers/offers.module#OffersPageModule'
+                    },
+                    {
+                        path: 'new',
+                        loadChildren: './offers/new-offer/new-offer.module#NewOfferPageModule'
+                    },
+                    {
+                        path: 'edit/:placeId',
+                        loadChildren: './offers/edit-offer/edit-offer.module#EditOfferPageModule'
+                    },
+                    {
+                        path: ':placeId',
+                        loadChildren: './offers/offer-bookings/offer-bookings.module#OfferBookingsPageModule'
+                    }
                 ]
             },
             {
